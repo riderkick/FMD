72aacaf483916231c5b07bd92a27b64be211c521
riderkick <colivaja@gmail.com>
2015-07-11 23:39:43 +0800

    tdbdataprocess, change convert db commit rate to 2000

--- baseunits/uData.pas
+++ baseunits/uData.pas
@@ -296,7 +296,7 @@ begin
             Status[i], StringBreaks(Summary[i]), StrToIntDef(Param[i, DATA_PARAM_NUMCHAPTER], 1),
             {%H-}Integer(JDN[i])-3);
           Inc(rcount);
-          if rcount > 499 then
+          if rcount >= 2000 then
           begin
             rcount := 0;
             dbdata.Commit;
