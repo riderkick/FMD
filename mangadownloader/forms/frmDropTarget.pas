3e4d5e907c9854a86521a607f2ad05cd5a282334
riderkick <colivaja@gmail.com>
2015-07-28 03:48:01 +0800

    frmdroptarget, get html format clipboard to parse multiple url

--- mangadownloader/forms/frmDropTarget.pas
+++ mangadownloader/forms/frmDropTarget.pas
@@ -5,8 +5,8 @@ unit frmDropTarget;
 interface
 
 uses
-  Classes, Windows, SysUtils, ActiveX, comobj, Forms, Controls,
-  ExtCtrls, Menus, LCLType, DefaultTranslator;
+  Classes, Windows, SysUtils, ActiveX, comobj, HTMLUtil, Forms, Controls,
+  ExtCtrls, Menus, LCLType, DefaultTranslator, uBaseUnit;
 
 type
 
@@ -34,14 +34,26 @@ type
     { private declarations }
     md: Boolean;
     x0, y0: Integer;
+    FCanDrop: Boolean;
     // IDropTarget
+    function MakeFormatEtc(const Fmt: TClipFormat): TFormatEtc;
+    function CursorEffect(const AllowedEffects: LongInt;
+      const KeyState: Integer): LongInt;
+    function CanDrop(const DataObj: IDataObject): Boolean;
+    function GetTextFromObj(const DataObj: IDataObject;
+      const Fmt: TClipFormat): String;
+    function GetWideTextFromObj(const DataObj: IDataObject;
+      const Fmt: TClipFormat): String;
+    function GetURLsFromHTML(const S: String): String;
+    function ParseDataObj(const DataObj: IDataObject;
+      const Fmt: TClipboardFormat): String;
     function DragEnter(const dataObj: IDataObject; grfKeyState: DWORD;
-      pt: TPoint; var dwEffect: DWORD): HResult; stdcall;
-    function {%H-}DragOver(grfKeyState: DWORD; pt: TPoint;
+      {%H-}pt: TPoint; var dwEffect: DWORD): HResult; stdcall;
+    function {%H-}DragOver(grfKeyState: DWORD; {%H-}pt: TPoint;
       var dwEffect: DWORD): HResult; stdcall;
     function DragLeave: HResult; stdcall;
-    function Drop(const dataObj: IDataObject; grfKeyState: DWORD;
-      pt: TPoint; var dwEffect: DWORD): HResult; stdcall;
+    function Drop(const dataObj: IDataObject; {%H-}grfKeyState: DWORD;
+      {%H-}pt: TPoint; var {%H-}dwEffect: DWORD): HResult; stdcall;
   protected
     procedure CreateParams(var Params: TCreateParams); override;
   public
@@ -61,6 +73,9 @@ implementation
 
 uses
   frmMain;
+
+var
+  CF_HTML: TClipFormat;
 
 {$R *.lfm}
 
@@ -151,17 +166,150 @@ begin
   Self.Close;
 end;
 
+function TFormDropTarget.MakeFormatEtc(const Fmt: TClipFormat): TFormatEtc;
+begin
+  Result.cfFormat := Fmt;
+  Result.ptd := nil;
+  Result.dwAspect := DVASPECT_CONTENT;
+  Result.lindex := -1;
+  Result.tymed := TYMED_HGLOBAL;
+end;
+
+function TFormDropTarget.CursorEffect(const AllowedEffects: LongInt;
+  const KeyState: Integer): LongInt;
+begin
+  Result := DROPEFFECT_NONE;
+  if FCanDrop then
+  begin
+    if (KeyState and MK_SHIFT = MK_SHIFT) and
+      (DROPEFFECT_MOVE and AllowedEffects = DROPEFFECT_MOVE) then
+      Result := DROPEFFECT_MOVE
+    else if (DROPEFFECT_COPY and AllowedEffects = DROPEFFECT_COPY) then
+      Result := DROPEFFECT_COPY;
+  end;
+end;
+
+function TFormDropTarget.CanDrop(const DataObj: IDataObject): Boolean;
+begin
+  Result := DataObj.QueryGetData(MakeFormatEtc(CF_HTML)) = S_OK;
+  if not Result then
+    Result := DataObj.QueryGetData(MakeFormatEtc(CF_UNICODETEXT)) = S_OK;
+  if not Result then
+    Result := DataObj.QueryGetData(MakeFormatEtc(CF_TEXT)) = S_OK;
+end;
+
+function TFormDropTarget.GetTextFromObj(const DataObj: IDataObject;
+  const Fmt: TClipFormat): String;
+var
+  Medium: TStgMedium;
+  PText: PChar;
+begin
+  if DataObj.GetData(MakeFormatEtc(Fmt), Medium) = S_OK then
+  begin
+    Assert(Medium.tymed = MakeFormatEtc(Fmt).tymed);
+    try
+      PText := GlobalLock(Medium.hGlobal);
+      try
+        Result := PText;
+      finally
+        GlobalUnlock(Medium.hGlobal);
+      end;
+    finally
+      ReleaseStgMedium(Medium);
+    end;
+  end
+  else
+    Result := '';
+end;
+
+function TFormDropTarget.GetWideTextFromObj(const DataObj: IDataObject;
+  const Fmt: TClipFormat): String;
+var
+  Medium: TStgMedium;
+  PwText: PWideChar;
+begin
+  if DataObj.GetData(MakeFormatEtc(Fmt), Medium) = S_OK then
+  begin
+    Assert(Medium.tymed = MakeFormatEtc(Fmt).tymed);
+    try
+      PwText := GlobalLock(Medium.hGlobal);
+      try
+        Result := PwText;
+      finally
+        GlobalUnlock(Medium.hGlobal);
+      end;
+    finally
+      ReleaseStgMedium(Medium);
+    end;
+  end
+  else
+    Result := '';
+end;
+
+function TFormDropTarget.GetURLsFromHTML(const S: String): String;
+var
+  Parse, URls: TStringList;
+  i: Integer;
+  t: String;
+begin
+  Result := S;
+  if S = '' then Exit;
+  Parse:= TStringList.Create;
+  try
+    ParseHTML(S, Parse);
+    if Parse.Count > 0 then
+    begin
+      URls := TStringList.Create;
+      try
+        for i := 0 to Parse.Count -1 do
+        begin
+          if LowerCase(GetTagName(Parse[i])) = 'a' then
+          t := GetVal(Parse[i], 'href');
+          if Pos('javascript', t) <> 1 then
+            URls.Add(t);
+        end;
+        if URls.Count > 0 then
+        begin
+          RemoveDuplicateStrings(URls);
+          Result := URls.Text;
+        end;
+      finally
+        URls.Free;
+      end;
+    end;
+  finally
+    Parse.Free;
+  end;
+end;
+
+function TFormDropTarget.ParseDataObj(const DataObj: IDataObject;
+  const Fmt: TClipboardFormat): String;
+begin
+  if Fmt = CF_HTML then
+    Result := GetURLsFromHTML(GetTextFromObj(DataObj, Fmt))
+  else
+  if Fmt = CF_UNICODETEXT then
+    Result := GetWideTextFromObj(DataObj, Fmt)
+  else
+  if Fmt = CF_TEXT then
+    Result := GetTextFromObj(DataObj, Fmt)
+  else
+    Result := '';
+  Result := Trim(Result);
+end;
+
 function TFormDropTarget.DragEnter(const dataObj: IDataObject; grfKeyState: DWORD;
   pt: TPoint; var dwEffect: DWORD): HResult; stdcall;
 begin
-  dwEffect := DROPEFFECT_COPY;
+  FCanDrop := CanDrop(dataObj);
+  dwEffect := CursorEffect(dwEffect, grfKeyState);
   Result := S_OK;
 end;
 
 function TFormDropTarget.DragOver(grfKeyState: DWORD; pt: TPoint;
   var dwEffect: DWORD): HResult; stdcall;
 begin
-  dwEffect := DROPEFFECT_COPY;
+  dwEffect := CursorEffect(dwEffect, grfKeyState);
   Result := S_OK;
 end;
 
@@ -173,36 +321,19 @@ end;
 function TFormDropTarget.Drop(const dataObj: IDataObject; grfKeyState: DWORD;
   pt: TPoint; var dwEffect: DWORD): HResult; stdcall;
 var
-  aFmtEtc: TFORMATETC;
-  aStgMed: TSTGMEDIUM;
-  pData: PChar;
+  Enum: IEnumFORMATETC;
+  FmtEtc: TFORMATETC;
 begin
-  // Support dropping text on edit control
-  // Make certain the data rendering is available
-  if (dataObj = nil) then
-    raise Exception.Create('IDataObject Pointer is not valid!');
-  with aFmtEtc do
-  begin
-    cfFormat := CF_TEXT;
-    ptd := nil;
-    dwAspect := DVASPECT_CONTENT;
-    lindex := -1;
-    tymed := TYMED_HGLOBAL;
-  end;
-  // Get the data
-  OleCheck(dataObj.GetData(aFmtEtc, aStgMed));
-  try
-    // Lock the global memory handle to get a pointer to the data
-    pData := GlobalLock(aStgMed.hGlobal);
-    // Replace Text in the control you want
-    if Assigned(OnDropChekout) then
-      OnDropChekout(pData);
-  finally
-    // Finished with the pointer
-    GlobalUnlock(aStgMed.hGlobal);
-    // Free the memory
-    ReleaseStgMedium(aStgMed);
-  end;
+  OleCheck(DataObj.EnumFormatEtc(DATADIR_GET, Enum));
+  while Enum.Next(1, FmtEtc, nil) = S_OK do
+    if (FmtEtc.CfFormat = CF_HTML) or
+       (FmtEtc.CfFormat = CF_UNICODETEXT) or
+       (FmtEtc.CfFormat = CF_TEXT) then
+    begin
+      if Assigned(OnDropChekout) then
+        OnDropChekout(ParseDataObj(dataObj, FmtEtc.CfFormat));
+      Break;
+    end;
   Result := S_OK;
 end;
 
@@ -212,5 +343,8 @@ begin
   Params.ExStyle := Params.ExStyle and WS_EX_TOOLWINDOW and (not WS_EX_APPWINDOW);
   Params.WndParent := GetDesktopWindow;
 end;
+
+initialization
+  CF_HTML := RegisterClipboardFormat('HTML Format');
 
 end.
