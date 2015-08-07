9b16c00039ea7ac2151398bee41f6f9adffa34e6
riderkick <colivaja@gmail.com>
2015-08-07 18:33:21 +0800

    doujin!moe, add getdirectorypagenumber and getnameandlink

--- baseunits/modules/Doujinmoeus.pas
+++ baseunits/modules/Doujinmoeus.pas
@@ -12,6 +12,58 @@ implementation
 uses
   simplehtmltreeparser, xquery;
 
+function GetDirectoryPageNumber(var MangaInfo: TMangaInformation;
+  var Page: Integer; Module: TModuleContainer): Integer;
+begin
+  Page := 100;
+  Result := NO_ERROR;
+end;
+
+function GetNameAndLink(var MangaInfo: TMangaInformation;
+  const Names, Links: TStringList; const URL: String; Module: TModuleContainer): Integer;
+var
+  Source: TStringList;
+  Parser: TTreeParser;
+  s: String;
+  v:IXQValue;
+begin
+  Result := NET_PROBLEM;
+  if MangaInfo = nil then Exit;
+  Source := TStringList.Create;
+  try
+    s := 'get=' + IncStr(URL);
+    with MangaInfo.FHTTP.Document do begin
+      Clear;
+      Write(PChar(s)^, Length(s));
+    end;
+    if GetPage(MangaInfo.FHTTP, TObject(Source), Module.RootURL + '/ajax/newest.php',
+      3, 'POST') then
+    begin
+      Parser := TTreeParser.Create;
+      try
+        ParseHTMLTree(Parser, Source.Text);
+        if SelectXPathString('json(*)("success")', Parser) = 'true' then
+        begin
+          s := SelectXPathString('json(*)("top")("token")', Parser);
+          if s <> '' then
+          begin
+            Links.Add(s);
+            names.Add(SelectXPathString('json(*)("top")("name")', Parser));
+          end;
+          for v in SelectXPathIX('json(*)("newest")()("token")', Parser) do
+            Links.Add(v.toString);
+          for v in SelectXPathIX('json(*)("newest")()("name")', Parser) do
+            Names.Add(v.toString);
+        end;
+      finally
+        Parser.Free;
+      end;
+    end;
+  finally
+    Source.Free;
+  end;
+end;
+
 function GetInfo(var MangaInfo: TMangaInformation; const URL: String;
   const Reconnect: Integer; Module: TModuleContainer): Integer;
 var
@@ -41,9 +93,11 @@ begin
             //cover
             coverLink := SelectXPathString('//div[@id="gallery"]/djm/@thumb', Parser);
             //artist
-            artists := SelectXPathString('//*[@id="page_info"]/div[@class="right"]/table/tbody/tr/td/a', Parser);
+            artists := SelectXPathString(
+              '//*[@id="page_info"]/div[@class="right"]/table/tbody/tr/td/a', Parser);
             //summary
-            summary := SelectXPathString('//*[@id="page_info"]/div/div[@class="message"]', Parser);
+            summary := SelectXPathString(
+              '//*[@id="page_info"]/div/div[@class="message"]', Parser);
             //chapter
             if title <> '' then
             begin
@@ -106,8 +160,10 @@ begin
   begin
     Website := 'Doujin-Moe';
     RootURL := 'http://www.doujin-moe.us';
-    InformationAvailable := True;
+    SortedList := True;
     FavoriteAvailable := False;
+    OnGetDirectoryPageNumber := @GetDirectoryPageNumber;
+    OnGetNameAndLink := @GetNameAndLink;
     OnGetInfo := @GetInfo;
     OnGetPageNumber := @GetPageNumber;
   end;
