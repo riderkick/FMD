a3ff35366dcc42faa61ccc2cdb1f4757ae2cd9e5
riderkick <colivaja@gmail.com>
2015-12-14 13:42:01 +0800

    rewrite bato to getinfo and add login

--- baseunits/modules/Batoto.pas
+++ baseunits/modules/Batoto.pas
@@ -6,16 +6,23 @@ interface
 
 uses
   Classes, SysUtils, WebsiteModules, uData, uBaseUnit, uDownloadsManager,
-  synautil, HTMLUtil, RegExpr;
+  accountmanager, synautil, HTMLUtil, RegExpr;
 
 implementation
 
 const
+  modulename = 'Batoto';
+  urlroot = 'http://bato.to';
+  urllogin = 'https://bato.to/forums/index.php?app=core&module=global&section=login&do=process';
   dirurls: array [0..1] of String = (
     '/comic/_/sp/',
     '/comic/_/comics/');
   perpage = 50;
   dirparam = '?sort_col=record_saved&sort_order=desc&per_page=';
+
+var
+  locklogin: TRTLCriticalSection;
+  onlogin: Boolean = False;
 
 function GetDirectoryPageNumber(var MangaInfo: TMangaInformation;
   var Page: Integer; Module: TModuleContainer): Integer;
@@ -116,140 +123,133 @@ begin
   end;
 end;
 
+function CheckLogin(const source: string): Boolean;
+begin
+  if source = '' then Exit(False);
+  Result := (Pos('class=''logged_in''', source) > 0) or (Pos('class="logged_in"', source) > 0);
+end;
+
+function Login(http: THTTPSendThread): Boolean;
+var
+  query: TXQueryEngineHTML;
+  source: TStringList;
+  s, key, user, pass: string;
+begin
+  Result := False;
+  if http = nil then Exit;
+  if Account.Username[modulename] = '' then Exit;
+
+  if TryEnterCriticalsection(locklogin) > 0 then
+    with http do begin
+      onlogin := True;
+      Reset;
+      Cookies.Clear;
+      if Get(urlroot) then begin
+        source := TStringList.Create;
+        query := TXQueryEngineHTML.Create;
+        try
+          source.LoadFromStream(Document);
+          query.ParseHTML(source.Text);
+          key := query.XPathString('//input[@name="auth_key"]/@value');
+          if key <> '' then begin
+            user:= Account.Username[modulename];
+            pass:= Account.Password[modulename];
+            s := 'auth_key='+key+'&referer=https%3A%2F%2Fbato.to%2F'+'&ips_username='+user+'&ips_password='+pass+'&rememberMe=1';
+            Clear;
+            Headers.Values['Referer'] := ' https://bato.to/';
+            if Post(urllogin, s) then begin
+              if ResultCode = 200 then begin
+                Result := Cookies.Values['pass_hash'] <> '';
+                if Result then begin
+                  Account.Cookies[modulename] := GetCookies;
+                  Account.Status[modulename] := 'OK';
+                end else begin
+                  Account.Status[modulename] := 'Invalid user/password!';
+                end;
+                Account.Save;
+              end;
+            end;
+          end;
+        finally
+          query.Free;
+          source.Free
+        end;
+      end;
+      onlogin := False
+    end
+  else
+  begin
+    while onlogin do Sleep(1000);
+    if Result then http.Cookies.Text := Account.Cookies[modulename];
+  end;
+end;
+
 function GetInfo(var MangaInfo: TMangaInformation; const URL: String;
   const Reconnect: Integer; Module: TModuleContainer): Integer;
 var
-  Parse: TStringList;
+  source: TStringList;
+  query: TXQueryEngineHTML;
   info: TMangaInfo;
-
-  procedure ScanChapters(const StartIndex: Integer);
-  var
-    i: Integer;
-    addchap: Boolean;
-    s: String;
-  begin
-    for i := StartIndex to Parse.Count - 1 do
-    begin
-      if Pos('</table', Parse[i]) <> 0 then
-        Break
-      else
-      if (GetTagName(Parse[i]) = 'tr') and (Pos(' chapter_row', Parse[i]) <> 0) then
-      begin
-        addchap := True;
-        s := CommonStringFilter(Parse[i + 6]);
-        if (not OptionBatotoShowAllLang) then
-          addchap := (Pos(' lang_English ', Parse[i]) <> 0)
-        else
-          s += Format(' [%s]', [Trim(GetVal(Parse[i + 12], 'title'))]);
-        if addchap then
-        begin
-          if OptionBatotoShowScanGroup then
-            s += Format(' [%s]', [Trim(Parse[i + 19])]);
-          info.chapterLinks.Add(GetVal(Parse[i + 4], 'href'));
-          info.chapterName.Add(s);
-        end;
-      end;
-    end;
-    if info.chapterLinks.Count > 0 then
-      InvertStrings([info.chapterLinks, info.chapterName]);
-  end;
-
-  procedure ScanParse;
-  var
-    i, j: Integer;
-  begin
-    info.genres := '';
-    info.summary := '';
-    for i := 0 to Parse.Count - 1 do
-    begin
-      //title
-      if info.title = '' then
-        if (Pos('ipsType_pagetitle', Parse[i]) > 0) then
-          info.title := CommonStringFilter(Parse[i + 1]);
-
-      //cover
-      if info.coverLink = '' then
-        if (GetTagName(Parse[i]) = 'img') and
-          (Pos('max-height:500px', Parse[i]) > 0) then
-          info.coverLink := GetVal(Parse[i], 'src');
-
-      //author
-      if (Pos('Author:', Parse[i]) > 0) then
-        info.authors := Trim(Parse[i + 5]);
-
-      //artist
-      if (Pos('Artist:', parse[i]) > 0) then
-        info.artists := Trim(Parse[i + 5]);
-
-      //genres
-      if Pos('/search?genres=', Parse[i]) > 0 then
-      begin
-        if Pos('</span', Parse[i + 4]) > 0 then
-          if info.genres = '' then
-            info.genres := Trim(Parse[i + 3])
-          else
-            info.genres := info.genres + ', ' + Parse[i + 3];
-      end;
-
-      //summary
-      if (Pos('Description:', Parse[i]) <> 0) then
-      begin
-        info.summary := '';
-        for j := i + 3 to Parse.Count - 1 do
-        begin
-          if Pos('</td>', Parse[j]) <> 0 then
-            Break
-          else
-          if Pos('<', Parse[j]) <> 1 then
-          begin
-            if info.summary <> '' then
-              info.summary += LineEnding;
-            info.summary += StringFilter(Parse[j]);
-          end;
-        end;
-      end;
-
-      //status
-      if (Pos('Status:', Parse[i]) > 0) then
-      begin
-        if (i + 4 < Parse.Count - 1) and
-          (Pos('Ongoing', Parse[i + 4]) > 0) then
-          info.status := '1'   // ongoing
-        else
-          info.status := '0';  // completed
-      end;
-
-      //chapters
-      if (GetTagName(Parse[i]) = 'table') and
-        (GetVal(Parse[i], 'class') = 'ipb_table chapters_list') then
-      begin
-        ScanChapters(i);
-        Break;
-      end;
-    end;
-  end;
-
+  v: IXQValue;
+  s: String;
+  i: Integer;
 begin
+  if MangaInfo = nil then Exit(UNKNOWN_ERROR);
   Result := NET_PROBLEM;
-  if MangaInfo = nil then Exit;
   info := MangaInfo.mangaInfo;
   info.website := Module.Website;
   info.url := FillHost(Module.RootURL, URL);
-  Parse := TStringList.Create;
-  try
-    if MangaInfo.GetPage(TObject(Parse), info.url, Reconnect) then
-    begin
-      Result := INFORMATION_NOT_FOUND;
-      ParseHTML(Parse.Text, Parse);
-      if Parse.Count > 0 then
-      begin
-        Result := NO_ERROR;
-        ScanParse;
+  while onlogin do Sleep(1000);
+  MangaInfo.FHTTP.Cookies.Text := Account.Cookies[modulename];
+  if MangaInfo.FHTTP.GET(info.url) then begin
+    Result := NO_ERROR;
+    source := TStringList.Create;
+    query := TXQueryEngineHTML.Create;
+    try
+      source.LoadFromStream(MangaInfo.FHTTP.Document);
+      if not CheckLogin(source.Text) then begin
+        if Login(MangaInfo.FHTTP) then begin
+          MangaInfo.FHTTP.GET(info.url, source);
+        end;
       end;
+
+      query.ParseHTML(source.Text);
+      with info do begin
+        coverLink := Query.XPathString('//div[@class="ipsBox"]//img/@src');
+        if title = '' then
+          title := Query.XPathString('//h1[@class="ipsType_pagetitle"]');
+
+        for v in query.XPath('//table[@class="ipb_table"]//tr') do begin
+          s := v.toString;
+          if Pos('Author:', s) > 0 then authors:= GetRightValue('Author:', s)
+          else if Pos('Artist:', s) > 0 then artists:= GetRightValue('Artist:', s)
+          else if Pos('Description:', s) > 0 then summary:= GetRightValue('Description:', s)
+          else if Pos('Status:', s) > 0 then begin
+            if Pos('Ongoing', s) > 0 then status := '1'
+            else status := '0';
+          end;
+        end;
+        v := query.XPath('//table[@class="ipb_table"]//tr[starts-with(*, "Genres:")]/td/a');
+        if v.Count > 0 then begin
+          genres := '';
+          for i := 0 to v.Count do AddCommaString(genres, v.get(i).toString);
+        end;
+
+        if OptionBatotoShowAllLang then
+          s := '//table[@class="ipb_table chapters_list"]//tr[starts-with(@class, "row lang")]/td[1]/a'
+        else s := '//table[@class="ipb_table chapters_list"]//tr[starts-with(@class, "row lang_English")]/td[1]/a';
+
+        for v in query.XPath(s) do begin
+          chapterLinks.Add(v.toNode.getAttribute('href'));
+          chapterName.Add(v.toString);
+        end;
+        InvertStrings([chapterLinks, chapterName])
+      end;
+    finally
+      query.Free;
+      source.Free;
     end;
-  finally
-    Parse.Free;
-  end;
+  end else Result := INFORMATION_NOT_FOUND;
 end;
 
 function GetPageNumber(var DownloadThread: TDownloadThread; const URL: String;
@@ -327,8 +327,8 @@ procedure RegisterModule;
 begin
   with AddModule do
   begin
-    Website := 'Batoto';
-    RootURL := 'http://bato.to';
+    Website := modulename;
+    RootURL := urlroot;
     MaxTaskLimit := 1;
     MaxConnectionLimit := 3;
     SortedList := True;
@@ -343,6 +343,10 @@ begin
 end;
 
 initialization
+  InitCriticalSection(locklogin);
   RegisterModule;
+
+finalization
+  DoneCriticalsection(locklogin);
 
 end.
