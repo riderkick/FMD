af64ff3e0239c1c55a29ddf2109407b4bd47d7f7
riderkick <colivaja@gmail.com>
2016-01-10 15:14:19 +0800

    added mangastreamto module
    fix #154

---
+++ baseunits/modules/MangaStreamTo.pas
@@ -0,0 +1,132 @@
+unit MangaStreamTo;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, WebsiteModules, uData, uBaseUnit, uDownloadsManager,
+  RegExpr, synautil;
+
+implementation
+
+const
+  dirurl='/series.html';
+
+function GetNameAndLink(var MangaInfo: TMangaInformation;
+  const ANames, ALinks: TStringList; const AURL: String; Module: TModuleContainer): Integer;
+var
+  query: TXQueryEngineHTML;
+  v: IXQValue;
+  s: String;
+begin
+  Result:=NET_PROBLEM;
+  if MangaInfo=nil then Exit(UNKNOWN_ERROR);
+  if MangaInfo.FHTTP.GET(Module.RootURL+dirurl) then begin
+    Result:=NO_ERROR;
+    query:=TXQueryEngineHTML.Create;
+    try
+      query.ParseHTML(StreamToString(MangaInfo.FHTTP.Document));
+      for v in query.XPath('//table[@id="page-list"]//tr/td/p/a') do begin
+        ALinks.Add(v.toNode.getAttribute('href'));
+        ANames.Add(v.toString);
+      end;
+    finally
+      query.Free;
+    end;
+  end;
+end;
+
+function GetInfo(var MangaInfo: TMangaInformation; const AURL: String;
+  const Reconnect: Integer; Module: TModuleContainer): Integer;
+var
+  query: TXQueryEngineHTML;
+  v: IXQValue;
+begin
+  Result:=NET_PROBLEM;
+  if MangaInfo=nil then Exit(UNKNOWN_ERROR);
+  with MangaInfo.FHTTP,MangaInfo.mangaInfo do begin
+    if GET(FillHost(Module.RootURL,AURL)) then begin
+      Result:=NO_ERROR;
+      query:=TXQueryEngineHTML.Create;
+      try
+        query.ParseHTML(StreamToString(Document));
+        if title=''then title:=query.XPathString('//div[@id="content-main"]/h1');
+        for v in query.XPath('//table[@class="ch-table"]/tbody/tr/td/a') do begin
+          chapterLinks.Add(v.toNode.getAttribute('href'));
+          chapterName.Add(v.toString);
+        end;
+      finally
+        query.Free;
+      end;
+    end;
+  end;
+end;
+
+function GetPageNumber(var DownloadThread: TDownloadThread; const AURL: String;
+  Module: TModuleContainer): Boolean;
+var
+  query: TXQueryEngineHTML;
+begin
+  Result:=False;
+  if DownloadThread=nil then Exit;
+  with DownloadThread.FHTTP,DownloadThread.manager.container do begin
+    PageLinks.Clear;
+    PageContainerLinks.Clear;
+    PageNumber := 0;
+    if GET(FillHost(Module.RootURL,AURL)) then begin
+      Result:=True;
+      query:=TXQueryEngineHTML.Create;
+      try
+        query.ParseHTML(StreamToString(Document));
+        PageNumber:=query.XPath('//select[@id="id_page"]/option').Count;
+      finally
+        query.Free;
+      end;
+    end;
+  end;
+end;
+
+function GetImageURL(var DownloadThread: TDownloadThread; const AURL: String;
+  Module: TModuleContainer): Boolean;
+var
+  query: TXQueryEngineHTML;
+  s: String;
+begin
+  Result:=False;
+  if DownloadThread=nil then Exit;
+  with DownloadThread.manager.container,DownloadThread.FHTTP do begin
+    s:=AURL;
+    s:=ReplaceRegExpr('\.html?$',s,'',False);
+    s+='-page-'+IncStr(DownloadThread.workCounter)+'.html';
+    if GET(FillHost(Module.RootURL,s)) then begin
+      Result:=True;
+      query:=TXQueryEngineHTML.Create;
+      try
+        query.ParseHTML(StreamToString(Document));
+        PageLinks[DownloadThread.workCounter]:=query.XPathString('//img[@class="manga-page center-block"]/@src');
+      finally
+        query.Free;
+      end;
+    end;
+  end;
+end;
+
+procedure RegisterModule;
+begin
+  with AddModule do
+  begin
+    Website:='MangaStreamTo';
+    RootURL:='http://www.mangastream.to';
+    InformationAvailable:=False;
+    OnGetNameAndLink:=@GetNameAndLink;
+    OnGetInfo:=@GetInfo;
+    OnGetPageNumber:=@GetPageNumber;
+    OnGetImageURL:=@GetImageURL;
+  end;
+end;
+
+initialization
+  RegisterModule;
+
+end.
