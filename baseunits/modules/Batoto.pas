f6d325a5e56acc938c15c39f0093c2b0c2bd87dc
riderkick <colivaja@gmail.com>
2016-01-18 20:12:36 +0800

    batoto, change update list per page to default

--- baseunits/modules/Batoto.pas
+++ baseunits/modules/Batoto.pas
@@ -17,7 +17,7 @@ const
   dirurls: array [0..1] of String = (
     '/comic/_/sp/',
     '/comic/_/comics/');
-  perpage = 1000;
+  perpage = 50;
   dirparam = '?sort_col=record_saved&sort_order=desc&per_page=';
 
 var
