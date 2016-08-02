624beb5f0b9ae9df59b4bab86918b7a7bcfca92d
riderkick <colivaja@gmail.com>
2016-08-03 00:09:47 +0800

    added besen as package and disable debug info

---
+++ 3rd/BESENPkg.pas
@@ -0,0 +1,41 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit BESENPkg;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  BESEN, BESENArrayUtils, BESENASTNodes, BESENBaseObject, BESENCharset, BESENCode, 
+  BESENCodeContext, BESENCodeGeneratorContext, BESENCodeJIT, BESENCodeJITx64, 
+  BESENCodeJITx86, BESENCodeSnapshot, BESENCollector, BESENCollectorObject, 
+  BESENCompiler, BESENConstants, BESENContext, BESENDateUtils, 
+  BESENDeclarativeEnvironmentRecord, BESENDecompiler, BESENDoubleList, 
+  BESENEnvironmentRecord, BESENErrors, BESENEvalCache, BESENEvalCacheItem, 
+  BESENGarbageCollector, BESENGlobals, BESENHashMap, BESENHashUtils, 
+  BESENInt64SelfBalancedTree, BESENIntegerList, BESENKeyIDManager, BESENLexer, 
+  BESENLexicalEnvironment, BESENLocale, BESENNativeCodeMemoryManager, BESENNativeObject, 
+  BESENNumberUtils, BESENObject, BESENObjectArgGetterFunction, 
+  BESENObjectArgSetterFunction, BESENObjectArray, BESENObjectArrayConstructor, 
+  BESENObjectArrayPrototype, BESENObjectBindingFunction, BESENObjectBoolean, 
+  BESENObjectBooleanConstructor, BESENObjectBooleanPrototype, BESENObjectConsole, 
+  BESENObjectConstructor, BESENObjectDate, BESENObjectDateConstructor, 
+  BESENObjectDatePrototype, BESENObjectDeclaredFunction, BESENObjectEnvironmentRecord, 
+  BESENObjectError, BESENObjectErrorConstructor, BESENObjectErrorPrototype, 
+  BESENObjectFunction, BESENObjectFunctionArguments, BESENObjectFunctionConstructor, 
+  BESENObjectFunctionPrototype, BESENObjectGlobal, BESENObjectJSON, BESENObjectMath, 
+  BESENObjectNativeFunction, BESENObjectNumber, BESENObjectNumberConstructor, 
+  BESENObjectNumberPrototype, BESENObjectPropertyDescriptor, BESENObjectPrototype, 
+  BESENObjectRegExp, BESENObjectRegExpConstructor, BESENObjectRegExpPrototype, 
+  BESENObjectString, BESENObjectStringConstructor, BESENObjectStringPrototype, 
+  BESENObjectThrowTypeErrorFunction, BESENOpcodes, BESENParser, BESENPointerList, 
+  BESENPointerSelfBalancedTree, BESENRandomGenerator, BESENRegExp, BESENRegExpCache, 
+  BESENScope, BESENSelfBalancedTree, BESENStringList, BESENStringTree, BESENStringUtils, 
+  BESENTypes, BESENUnicodeTables, BESENUtils, BESENValue, BESENValueContainer, 
+  BESENVersionConstants;
+
+implementation
+
+end.
