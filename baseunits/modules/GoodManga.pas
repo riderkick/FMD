bb4a6ca97186eab999a87a44049251790d6cdd78
riderkick <colivaja@gmail.com>
2017-01-21 17:49:32 +0800

    goodmaga, fixed mangainfo #454

--- baseunits/modules/GoodManga.pas
+++ baseunits/modules/GoodManga.pas
@@ -17,23 +17,15 @@ const
 function GetNameAndLink(const MangaInfo: TMangaInformation;
   const ANames, ALinks: TStringList; const AURL: String;
   const Module: TModuleContainer): Integer;
-var
-  v: IXQValue;
-  s: String;
 begin
   Result := NET_PROBLEM;
   if MangaInfo = nil then Exit(UNKNOWN_ERROR);
-  s := Module.RootURL + dirurl;
-  if MangaInfo.FHTTP.GET(s) then
+  if MangaInfo.FHTTP.GET(Module.RootURL + dirurl) then
   begin
     Result := NO_ERROR;
     with TXQueryEngineHTML.Create(MangaInfo.FHTTP.Document) do
       try
-        for v in XPath('//td/a') do
-        begin
-          ALinks.Add(v.toNode.getAttribute('href'));
-          ANames.Add(v.toString);
-        end;
+        XPathHREFAll('//td/a', ALinks, ANames);
       finally
         Free;
       end;
@@ -43,7 +35,7 @@ end;
 function GetInfo(const MangaInfo: TMangaInformation;
   const AURL: String; const Module: TModuleContainer): Integer;
 var
-  v: IXQValue;
+  s: String;
 begin
   Result := NET_PROBLEM;
   if MangaInfo = nil then Exit(UNKNOWN_ERROR);
@@ -57,17 +49,23 @@ begin
           coverLink := XPathString('//img[@id="series_image"]/@src');
           if coverLink <> '' then coverLink := MaybeFillHost(Module.RootURL, coverLink);
           if title = '' then title := XPathString('//div[@class="right_col"]/h1');
-          status := MangaInfoStatusIfPos(
-            XPathString('//*[@class="info"]/p/*[@class="data"][starts-with(.,"Status:")]/following-sibling::*[@class="desc"][1]'),
-            'Ongoing',
-            'Complete');
-          artists := XPathStringAll('//*[@class="info"]/p/*[@class="data"][starts-with(.,"Artist:")]/following-sibling::*[@class="desc"][1]/a');
-          authors := XPathStringAll('//*[@class="info"]/p/*[@class="data"][starts-with(.,"Author:")]/following-sibling::*[@class="desc"][1]/a');
-          for v in XPath('//div[@id="chapters"]/ul/li/a') do
+          authors := XPathString('//div[@id="series_details"]/div/span[starts-with(.,"Authors:")]/following-sibling::text()');
+          summary := XPathString('//div[@id="series_details"]/div//span[@id="full_notes"]');
+          if summary = '' then
+            summary := XPathString('//div[@id="series_details"]/div/span[starts-with(.,"Synopsis:")]/following-sibling::*');
+          status := MangaInfoStatusIfPos(XPathString('//div[@id="series_details"]/div/span[starts-with(.,"Status:")]/following-sibling::text()'));
+          genres := XPathString('//div[@id="series_details"]/div/span[starts-with(.,"Genres:")]/string-join(following-sibling::*,", ")');
+          while true do
           begin
-            chapterLinks.Add(v.toNode.getAttribute('href'));
-            chapterName.Add(v.toString);
+            XPathHREFAll('//div[@id="chapters"]/ul/li/a', chapterLinks, chapterName);
+            if Thread.IsTerminated then Break;
+            s := XPathString('//ul[@class="pagination"]/li/a[.="Next"]/@href');
+            if (s <> '') and GET(MaybeFillHost(Module.RootURL, s)) then
+              ParseHTML(Document)
+            else
+              Break;
           end;
+          InvertStrings([chapterLinks,chapterName]);
         finally
           Free;
         end;
