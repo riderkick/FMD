9ca4945e2b12ded4baea9a379d7cd063759deb73
riderkick <colivaja@gmail.com>
2017-02-18 06:43:31 +0800

    added more imagehoster

--- baseunits/modules/ImageHoster.pas
+++ baseunits/modules/ImageHoster.pas
@@ -11,23 +11,37 @@ function GetImageHoster(const AHTTP: THTTPSendThread; const AURL: String): Strin
 
 implementation
 
+const
+  imghosts: array [0..2] of array [0..1] of string = (
+    ('imagetwist.com', '//img[@class="pic img img-responsive"]/@src'),
+    ('imgchili.net', '//img[@id="show_image"]/@src'),
+    ('imgbox.com', '//img[@id="img"]/@src ')
+    );
+
 function GetImageHoster(const AHTTP: THTTPSendThread; const AURL: String): String;
 var
   u: String;
+  i: Integer;
 begin
   Result := '';
   u := Trim(LowerCase(AURL));
   if u = '' then Exit;
-  if Pos('imagetwist.com', u) <> 0 then
+
+  for i := Low(imghosts) to High(imghosts) do
   begin
-    AHTTP.GET(AURL);
-    AHTTP.GET(AURL);
-    with TXQueryEngineHTML.Create(AHTTP.Document) do
-      try
-        Result := XPathString('//img[@class="pic img img-responsive"]/@src');
-      finally
-        Free;
-      end;
+    if Pos(imghosts[i, 0], u) <> 0 then
+    begin
+      AHTTP.GET(AURL);
+      if i = 0 then
+        AHTTP.GET(AURL);
+      with TXQueryEngineHTML.Create(AHTTP.Document) do
+        try
+          Result := XPathString(imghosts[i, 1]);
+        finally
+          Free;
+        end;
+      Break;
+    end;
   end;
 end;
 
