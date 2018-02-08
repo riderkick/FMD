aaa8b84301d1c8a1af6f258839aac5d2d217f9aa
riderkick <colivaja@gmail.com>
2018-02-08 11:27:14 +0800

    luaregex, use original method name
    for easier converting existing module

--- baseunits/lua/LuaRegex.pas
+++ baseunits/lua/LuaRegex.pas
@@ -11,6 +11,9 @@ procedure luaRegexRegister(L: Plua_State);
 
 implementation
 
+uses
+  LuaUtils;
+
 function re_exec(L: Plua_State): Integer; cdecl;
 begin
   lua_pushboolean(L, ExecRegExpr(lua_tostring(L, 1), lua_tostring(L, 2)));
@@ -23,18 +26,10 @@ begin
   Result := 1;
 end;
 
-const
-  methods: packed array [0..2] of luaL_Reg = (
-    (name: 'Exec'; func: @re_exec),
-    (name: 'Replace'; func: @re_replace),
-    (name: nil; func: nil)
-    );
-
 procedure luaRegexRegister(L: Plua_State);
 begin
-  lua_newtable(L);
-  luaL_setfuncs(L, @methods, 0);
-  lua_setglobal(L, 'RegExpr');
+  luaPushFunctionGlobal(L, 'ExecRegExpr', @re_exec);
+  luaPushFunctionGlobal(L, 'ReplaceRegExpr', @re_replace);
 end;
 
 end.
