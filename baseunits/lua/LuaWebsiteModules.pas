d66a3bb86bd50bf67a77cde19e302fe7b8f59ff5
kmvi <kmvi@users.noreply.github.com>
2018-02-15 14:34:03 +0300

    LuaWebsiteModules, fix erroneous totaldirectory

--- baseunits/lua/LuaWebsiteModules.pas
+++ baseunits/lua/LuaWebsiteModules.pas
@@ -767,7 +767,6 @@ begin
     luaClassAddIntegerProperty(L, MetaTable, 'MaxTaskLimit', @Module.MaxTaskLimit);
     luaClassAddIntegerProperty(L, MetaTable, 'MaxConnectionLimit',
       @Module.MaxConnectionLimit);
-    luaClassAddIntegerProperty(L, MetaTable, 'TotalDirectory', @Module.TotalDirectory);
     luaClassAddIntegerProperty(L, MetaTable, 'ActiveTaskCount', @Module.ActiveTaskCount);
     luaClassAddIntegerProperty(L, MetaTable, 'ActiveConnectionCount',
       @Module.ActiveConnectionCount);
