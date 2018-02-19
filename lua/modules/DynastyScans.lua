046843b6dff18e19a279695c4986859e2a54de54
riderkick <colivaja@gmail.com>
2018-02-20 07:34:35 +0800

    dynastiscans, fix manga info (#1026)

--- lua/modules/DynastyScans.lua
+++ lua/modules/DynastyScans.lua
@@ -2,13 +2,13 @@ function getinfo()
   mangainfo.url=MaybeFillHost(module.RootURL, url)
   if http.get(mangainfo.url) then
     x=TXQuery.Create(http.document)	
-    mangainfo.title=x.xpathstring('//*[@class="tag-title"]')
+    mangainfo.title=x.xpathstring('//h2[@class="tag-title"]/b')
     mangainfo.coverlink=MaybeFillHost(module.rooturl,x.xpathstring('//img[@class="thumbnail"]/@src'))
-    mangainfo.authors=x.xpathstring('//a[contains(@href,"/title/")]')
+    mangainfo.authors=x.xpathstring('string-join(//a[contains(@href,"/authors/")],", ")')
     mangainfo.genres=x.xpathstringall('//*[@class="label" or @class="doujin_tags"]')
-    mangainfo.status=MangaInfoStatusIfPos(x.xpathstring('//small'))
+    mangainfo.status=MangaInfoStatusIfPos(x.xpathstring('//h2[@class="tag-title"]/small'))
     mangainfo.summary=x.xpathstring('//*[@class="description"]')
-    x.xpathhrefall('//dl[@class="chapter-list"]/dd/a',mangainfo.chapterlinks,mangainfo.chapternames)
+    x.xpathhrefall('//dl[@class="chapter-list"]/dd/a[1]',mangainfo.chapterlinks,mangainfo.chapternames)
     return no_error
   else
     return net_problem
