951f30e5523705a8b4c8b728874576abe28a77a2
riderkick <colivaja@gmail.com>
2018-02-22 20:08:24 +0800

    websitemodulesettings, add default property

--- baseunits/WebsiteModules.pas
+++ baseunits/WebsiteModules.pas
@@ -85,14 +85,14 @@ type
     FUpdateListNumberOfThread: Integer;
     FUserAgent: String;
   published
-    property MaxTaskLimit: Integer read FMaxTaskLimit write FMaxTaskLimit;
-    property MaxThreadPerTaskLimit: Integer read FMaxThreadPerTaskLimit write FMaxThreadPerTaskLimit;
-    property MaxConnectionLimit: Integer read FMaxConnectionLimit write FMaxConnectionLimit;
-    property UpdateListNumberOfThread: Integer read FUpdateListNumberOfThread write FUpdateListNumberOfThread;
-    property UpdateListDirectoryPageNumber: Integer read FUpdateListDirectoryPageNumber write FUpdateListDirectoryPageNumber;
+    property MaxTaskLimit: Integer read FMaxTaskLimit write FMaxTaskLimit default 0;
+    property MaxThreadPerTaskLimit: Integer read FMaxThreadPerTaskLimit write FMaxThreadPerTaskLimit default 0;
+    property MaxConnectionLimit: Integer read FMaxConnectionLimit write FMaxConnectionLimit default 0;
+    property UpdateListNumberOfThread: Integer read FUpdateListNumberOfThread write FUpdateListNumberOfThread default 0;
+    property UpdateListDirectoryPageNumber: Integer read FUpdateListDirectoryPageNumber write FUpdateListDirectoryPageNumber default 0;
     property UserAgent: String read FUserAgent write FUserAgent;
     property Cookies: String read FCookies write FCookies;
-    property ProxyType: TProxyType read FProxyType write FProxyType;
+    property ProxyType: TProxyType read FProxyType write FProxyType default ptDefault;
     property ProxyHost: String read FProxyHost write FProxyHost;
     property ProxyPort: String read FProxyPort write FProxyPort;
     property ProxyUsername: String read FProxyUsername write FProxyUsername;
