8d591e14e61159a823d782e0a9d4bb86a241bec3
kmvi <kmvi@users.noreply.github.com>
2018-03-01 12:52:39 +0300

    add TrashScanlations [EN-SC] fixes #1041

--- lua/modules/rawdevart.lua
+++ lua/modules/rawdevart.lua
@@ -19,7 +19,11 @@ end
 
 function getpagenumber()
   task.pagelinks.clear()
-  if http.get(MaybeFillHost(module.rooturl, url)) then
+  local aurl = MaybeFillHost(module.rooturl, url)
+  if Pos('style=list', aurl) == 0 then
+    aurl = aurl .. '?style=list'
+  end
+  if http.get(aurl) then
     x=TXQuery.Create(http.Document)
     v=x.xpathstringall('//div[@class="page-break"]/img/@src', task.pagelinks)
   else
@@ -32,7 +36,6 @@ local perpage = 100
 
 function getnameandlink()
   local q = 'action=madara_load_more&page='.. url ..'&template=madara-core%2Fcontent%2Fcontent-archive&vars%5Bpost_type%5D=wp-manga&vars%5Berror%5D=&vars%5Bm%5D=&vars%5Bp%5D=0&vars%5Bpost_parent%5D=&vars%5Bsubpost%5D=&vars%5Bsubpost_id%5D=&vars%5Battachment%5D=&vars%5Battachment_id%5D=0&vars%5Bname%5D=&vars%5Bstatic%5D=&vars%5Bpagename%5D=&vars%5Bpage_id%5D=0&vars%5Bsecond%5D=&vars%5Bminute%5D=&vars%5Bhour%5D=&vars%5Bday%5D=0&vars%5Bmonthnum%5D=0&vars%5Byear%5D=0&vars%5Bw%5D=0&vars%5Bcategory_name%5D=&vars%5Btag%5D=&vars%5Bcat%5D=&vars%5Btag_id%5D=&vars%5Bauthor%5D=&vars%5Bauthor_name%5D=&vars%5Bfeed%5D=&vars%5Btb%5D=&vars%5Bpaged%5D=1&vars%5Bmeta_key%5D=&vars%5Bmeta_value%5D=&vars%5Bpreview%5D=&vars%5Bs%5D=&vars%5Bsentence%5D=&vars%5Btitle%5D=&vars%5Bfields%5D=&vars%5Bmenu_order%5D=&vars%5Bembed%5D=&vars%5Bignore_sticky_posts%5D=false&vars%5Bsuppress_filters%5D=false&vars%5Bcache_results%5D=true&vars%5Bupdate_post_term_cache%5D=true&vars%5Blazy_load_term_meta%5D=true&vars%5Bupdate_post_meta_cache%5D=true&vars%5Bposts_per_page%5D='.. tostring(perpage) ..'&vars%5Bnopaging%5D=false&vars%5Bcomments_per_page%5D=50&vars%5Bno_found_rows%5D=false&vars%5Border%5D=ASC&vars%5Borderby%5D=post_title&vars%5Btemplate%5D=archive&vars%5Bsidebar%5D=full&vars%5Bpost_status%5D=publish'
-  
   if http.post(module.rooturl .. '/wp-admin/admin-ajax.php', q) then
     if http.headers.values['Content-Length'] == '0' then return no_error end
     x = TXQuery.Create(http.Document)
@@ -45,13 +48,19 @@ function getnameandlink()
   end
 end
 
-function Init()
+function AddWebsiteModule(name, url, category)
   local m = NewModule()
-  m.website = 'Rawdevart'
-  m.rooturl = 'https://rawdevart.com'
-  m.category = 'Raw'
-  m.lastupdated='February 22, 2018'
+  m.website = name
+  m.rooturl = url
+  m.category = category
+  m.lastupdated = 'March 1, 2018'
   m.ongetinfo='getinfo'
   m.ongetpagenumber='getpagenumber'
   m.ongetnameandlink='getnameandlink'
+  return m
+end
+
+function Init()
+  AddWebsiteModule('Rawdevart', 'https://rawdevart.com', 'Raw')
+  AddWebsiteModule('TrashScanlations', 'http://trashscanlations.com', 'English-Scanlation')
 end
