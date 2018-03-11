e6df5ba83373cab2377929bcf8e1fe998d59053f
kmvi <kmvi@users.noreply.github.com>
2018-03-11 07:50:48 +0300

    imgur, fix download fixes #1114

--- lua/modules/Imgur.lua
+++ lua/modules/Imgur.lua
@@ -17,14 +17,30 @@ function taskstart()
 end
 
 function getpagenumber()
-  if http.get(MaybeFillHost(module.rooturl,url)) then
-    x=TXQuery.Create(http.Document)
-    x.xpathStringAll('//div[contains(@class,"post-image")]/a/img/concat("https:",@src)', task.pagelinks)
-    return true
+  local hash = url:match('/a/(.+)/?')
+  if hash ~= nil then
+    -- album
+    if http.get(module.rooturl .. '/ajaxalbums/getimages/' .. hash .. '/hit.json') then
+      local x = TXQuery.Create(http.Document)
+      local v = x.xpath('json(*).data.images()')
+      for i = 1, v.count do
+        local v1 = v.get(i)
+        task.pagelinks.add('https://i.imgur.com/' .. x.xpathstring('hash', v1) .. x.xpathstring('ext', v1))
+      end
+      return true
+    else
+      return false
+    end
   else
-    return false
+    -- single image
+    if http.get(MaybeFillHost(module.rooturl,url)) then
+      x=TXQuery.Create(http.Document)
+      x.xpathStringAll('//div[contains(@class,"post-image")]/a/img/concat("https:",@src)', task.pagelinks)
+      return true
+    else
+      return false
+    end
   end
-  return true
 end
 
 function Init()
