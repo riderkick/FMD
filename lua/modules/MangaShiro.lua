aca0eeb322faa2a373a092bd277ca6989bb24dfc
kmvi <kmvi@users.noreply.github.com>
2018-03-29 09:42:10 +0300

    add KomikCast [id] #874 #1139

--- lua/modules/MangaShiro.lua
+++ lua/modules/MangaShiro.lua
@@ -1,13 +1,22 @@
 function getinfo()
   mangainfo.url=MaybeFillHost(module.RootURL, url)
   if http.get(mangainfo.url) then
-    x=TXQuery.Create(http.document)	
-    mangainfo.title=x.xpathstring('//h1[@itemprop="name"]')
-    mangainfo.coverlink=MaybeFillHost(module.RootURL, x.xpathstring('//div[@class="imgdesc"]/img/@src'))
-    mangainfo.authors=x.xpathstring('//div[@class="listinfo"]//li[starts-with(.,"Author")]/substring-after(.,":")')
-    mangainfo.genres=x.xpathstring('//div[@class="listinfo"]//li[starts-with(.,"Genre")]/substring-after(.,":")')
-    mangainfo.status=MangaInfoStatusIfPos(x.xpathstring('//div[@class="listinfo"]//li[starts-with(.,"Status")]'))
-    mangainfo.summary=x.xpathstring('//*[@class="desc"]/string-join(.//text(),"")')
+    x=TXQuery.Create(http.document)
+    if module.website == 'KomikCast' then
+      mangainfo.title=x.xpathstring('//div[@class="mangainfo"]/h1')
+      mangainfo.coverlink=MaybeFillHost(module.RootURL, x.xpathstring('//div[@class="topinfo"]/img/@src'))
+      mangainfo.authors=x.xpathstring('//div[@class="topinfo"]//tr[contains(th, "Author")]/td')
+      mangainfo.genres=x.xpathstringall('//div[@class="topinfo"]//tr[contains(th, "Genres")]/td/a')
+      mangainfo.status=MangaInfoStatusIfPos(x.xpathstring('//div[@class="topinfo"]//tr[contains(th, "Status")]/td'))
+      mangainfo.summary=x.xpathstringall('//*[@class="sin"]/p/text()', '')
+    else
+      mangainfo.title=x.xpathstring('//h1[@itemprop="name"]')
+      mangainfo.coverlink=MaybeFillHost(module.RootURL, x.xpathstring('//div[@class="imgdesc"]/img/@src'))
+      mangainfo.authors=x.xpathstring('//div[@class="listinfo"]//li[starts-with(.,"Author")]/substring-after(.,":")')
+      mangainfo.genres=x.xpathstring('//div[@class="listinfo"]//li[starts-with(.,"Genre")]/substring-after(.,":")')
+      mangainfo.status=MangaInfoStatusIfPos(x.xpathstring('//div[@class="listinfo"]//li[starts-with(.,"Status")]'))
+      mangainfo.summary=x.xpathstring('//*[@class="desc"]/string-join(.//text(),"")')
+    end
     x.xpathhrefall('//div[@class="cl"]//li/span[1]/a', mangainfo.chapterlinks, mangainfo.chapternames)
     InvertStrings(mangainfo.chapterlinks,mangainfo.chapternames)
     return no_error
@@ -31,6 +40,7 @@ function getnameandlink()
   local dirurl = '/manga-list/'
   if module.website == 'MangaShiro' then dirurl = '/daftar-manga/'
   elseif module.website == 'KomikStation' then dirurl = '/daftar-komik/'
+  elseif module.website == 'KomikCast' then dirurl = '/daftar-komik/?list'
   elseif module.website == 'MangaKid' then dirurl = '/manga-lists/'
   end
   if http.get(module.rooturl..dirurl) then
@@ -64,4 +74,5 @@ function Init()
   AddWebsiteModule('Mangavy', 'https://mangavy.com')
   AddWebsiteModule('KomikStation', 'http://www.komikstation.com')
   AddWebsiteModule('MangaKid', 'http://mangakid.net')
+  AddWebsiteModule('KomikCast', 'https://komikcast.com')
 end
