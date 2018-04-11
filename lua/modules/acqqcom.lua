ef829172d11fd004effa649793a754a806174fe4
kmvi <kmvi@users.noreply.github.com>
2018-04-11 19:25:19 +0300

    cleanup

--- lua/modules/acqqcom.lua
+++ lua/modules/acqqcom.lua
@@ -13,54 +13,12 @@
   end
 end
 
-function decode1(str)
-  local _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-  str = str:gsub('[^A-Za-z0-9%+/=]', '')
-  local a = ''
-  local b, d, h, f, g, e = 0, 0, 0, 0, 0, 0
-  while e < str:len() do
-    e = e + 1; b = string.find(_keyStr, str:sub(e, e)); if b == nil then b = -1 else b = b - 1 end;
-    e = e + 1; d = string.find(_keyStr, str:sub(e, e)); if d == nil then d = -1 else d = d - 1 end;
-    e = e + 1; f = string.find(_keyStr, str:sub(e, e)); if f == nil then f = -1 else f = f - 1 end;
-    e = e + 1; g = string.find(_keyStr, str:sub(e, e)); if g == nil then g = -1 else g = g - 1 end;
-    b = b << 2 | d >> 4
-    d = (d & 15) << 4 | f >> 2
-    h = (f & 3) << 6 | g
-    a = a .. string.char(b)
-    if f ~= 64 then a = a .. string.char(d); end
-    if g ~= 64 then a = a .. string.char(h); end
-  end
-  function utf8_decode(c)
-    local a, b, d, c1, c2 = '', 0, 0, 0, 0
-    while b < c:len() do
-      d = c:byte(b+1)
-      if 128 > d then
-        a = a .. string.char(d)
-        b = b + 1
-      else
-        if (191 < d) and (224 > d) then
-          c2 = c:byte(b+2)
-          a = a .. string.char((d & 31) << 6 | c2 & 63)
-          b = b + 2
-        else
-          c2 = c:byte(b+2)
-          c3 = c:byte(b+3)
-          a = a .. string.char((d & 15) << 12 | (c2 & 63) << 6 | c3 & 63)
-          b = b + 3
-        end
-      end
-    end
-    return a
-  end
-  return utf8_decode(a)
-end
-
 function getpagenumber()
   if http.get(MaybeFillHost(module.rooturl,url)) then
     x=TXQuery.Create(http.Document)
     local s = x.xpathstring('//script[contains(., "var DATA")]')
     s = GetBetween("= '", "',", s)
-    s = decode1(s:sub(2))
+    s = DecodeBase64(s:sub(2))
     x.parsehtml(s)
     x.xpathstringall('json(*).picture().url', task.pagelinks)
     return true
