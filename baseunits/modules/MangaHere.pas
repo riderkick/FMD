797d06d0309e9257db3fee735834d5b4579f2c37
kmvi <kmvi@users.noreply.github.com>
2018-04-14 09:50:46 +0300

    MangaHere, fix title

--- baseunits/modules/MangaHere.pas
+++ baseunits/modules/MangaHere.pas
@@ -30,7 +30,7 @@ begin
         for v in XPath('//a[@class="manga_info"]') do
         begin
           ALinks.Add(v.toNode.getAttribute('href'));
-          ANames.Add(v.toString);
+          ANames.Add(v.toNode.getAttribute('rel'));
         end;
       finally
         Free;
