46b9005bff5435b0eb78427c4f6326cef54aefad
kmvi <kmvi@users.noreply.github.com>
2019-01-09 07:18:28 +0300

    Pururin, fix all closes #1425 closes #1355

--- lua/modules/Pururin.lua
+++ lua/modules/Pururin.lua
@@ -1,12 +1,14 @@
+local domain = 'pururin.io'
+
 function GetInfo()
   mangainfo.url=MaybeFillHost(module.rooturl,url)
   if http.get(mangainfo.url) then
     x=TXQuery.Create(http.Document)
-    mangainfo.title=x.XPathString('//*[@class="gallery-info"]//*[@class="title"]')
-    mangainfo.coverLink=MaybeFillHost(module.rooturl, x.XPathString('//*[@class="cover"]//img/@src'))
-    mangainfo.artists = x.XPathString('//*[@class="gallery-info"]//*[@class="table"]//tr/td[starts-with(.,"Artist")]/following-sibling::td[1]/string-join(.//a,", ")')
-    mangainfo.genres = x.XPathString('//*[@class="gallery-info"]//*[@class="table"]/string-join(.//tr/td//a,", ")')
-    mangainfo.chapterlinks.add(x.XPathString('//a[@class="read-more"]/@href'))
+    mangainfo.title=x.XPathString('//*[@class="title"]/h1')
+    mangainfo.coverLink=MaybeFillHost(module.rooturl, x.XPathString('//*[@class="cover-wrapper"]//v-lazy-image/@src'))
+    mangainfo.artists = x.XPathStringAll('//table[contains(@class,"table-gallery-info")]//tr/td[contains(.,"Artist")]/following-sibling::td//a')
+    mangainfo.genres = x.XPathStringAll('//table[contains(@class,"table-gallery-info")]//tr/td[contains(.,"Contents")]/following-sibling::td//a')
+    mangainfo.chapterlinks.add(x.XPathString('//*[@class="gallery-action"]/a/@href'))
     mangainfo.chapternames.add(mangainfo.title)
     return no_error
   else
@@ -15,9 +17,17 @@ function GetInfo()
 end
 
 function GetPageNumber()
+  local path = 'https://api.' .. domain .. '/images'
   if http.get(MaybeFillHost(module.rooturl,url)) then
-    x=TXQuery.Create(http.Document)
-    x.XPathStringAll('json(//script[contains(.,"var chapters")]/substring-before(substring-after(.,"= "),";"))//image', task.pagelinks)
+    local x=TXQuery.Create(http.Document)
+    local s = x.xpathstring('//gallery-read/@gallery')
+    x.parsehtml(s)
+    local ext = x.xpathstring('json(*).image_extension')
+    local cnt = x.xpathstring('json(*).total_pages')
+    local id = x.xpathstring('json(*).id')
+    for i = 1, cnt do
+      task.pagelinks.add(string.format('%s/%s/%d.%s', path, id, i, ext))
+    end
     return true
   else
     return false
@@ -26,10 +36,10 @@ end
 
 function GetNameAndLink()
   if http.get(module.rooturl..'/browse/newest?page='..IncStr(url)) then
-    x=TXQuery.Create(http.Document)
-    v=x.xpath('//*[@class="gallery-listing"]/*[@class="row"]/a')
+    local x=TXQuery.Create(http.Document)
+    local v=x.xpath('//*[@class="row-gallery"]/a')
     for i=1,v.count do
-      v1 = v.get(i)
+      local v1 = v.get(i)
       links.add(v1.getAttribute('href'))
       names.add(x.xpathstring('.//*[@class="title"]/text()[1]', v1))
     end
@@ -41,8 +51,8 @@ end
 
 function getdirectorypagenumber()
   if http.GET(module.RootURL) then
-    x = TXQuery.Create(http.Document)
-    page = tonumber(x.XPathString('//*[@class="pagination"]/li[last()-1]'))
+    local x = TXQuery.Create(http.Document)
+    page = tonumber(x.XPathString('//ul[contains(@class,"pagination")]/li[last()-1]'))
     if page == nil then page = 1 end
     return true
   else
@@ -54,9 +64,8 @@ function Init()
   m=NewModule()
   m.category='H-Sites'
   m.website='Pururin'
-  m.rooturl='http://pururin.io'
+  m.rooturl='https://' .. domain
   m.sortedlist=true
-  m.lastupdated='February 14, 2018'
   m.ongetinfo='GetInfo'
   m.ongetpagenumber='GetPageNumber'
   m.ongetnameandlink='GetNameAndLink'
