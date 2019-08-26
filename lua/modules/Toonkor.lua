a3da74caf12f158efb3292dd3defd9006d5800a4
Slasar41 <agungtrianto41@outlook.com>
2019-08-27 01:54:58 +0700

    Website changed domain [Toonkor]

--- lua/modules/Toonkor.lua
+++ lua/modules/Toonkor.lua
@@ -62,7 +62,7 @@ function Init()
   local m = NewModule()
   m.category = 'Raw'
   m.Website = 'Toonkor'
-  m.RootURL = 'https://toonkor.guru'
+  m.RootURL = 'https://toonkor.id'
   m.ongetinfo='getinfo'
   m.ongetpagenumber='getpagenumber'
   m.ongetnameandlink='getnameandlink'
