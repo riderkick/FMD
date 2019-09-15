3923d530ba06d9dea984340d98ebe08b7956d5b8
Abid Zakaria <abidarmu@gmail.com>
2019-09-15 13:10:30 +0700

    fix chapter download #491

--- lua/modules/Lhscans.lua
+++ lua/modules/Lhscans.lua
@@ -32,7 +32,11 @@ function getpagenumber()
   local u = MaybeFillHost(module.rooturl, url)
   if http.get(u) then
     local x=TXQuery.Create(http.Document)
-    x.xpathstringall('//img[@class="chapter-img"]/@src', task.pagelinks)
+	if module.website == 'Lhscans' then
+		x.xpathstringall('//img[contains(@class, "chapter-img")]/@data-src', task.pagelinks)
+    	else
+    		x.xpathstringall('//img[@class="chapter-img"]/@src', task.pagelinks)
+	end
 	if module.website == 'Lhscans' or module.website == 'MangaHato' then task.pagecontainerlinks.text = u end
   else
     return false
