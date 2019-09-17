fc4dd016f63b38fe1c52334262f450ca730d0638
Abid Zakaria <abidarmu@gmail.com>
2019-09-18 00:47:31 +0700

    [HeavenManga] Change Url #500

--- lua/modules/HeavenManga.lua
+++ lua/modules/HeavenManga.lua
@@ -86,6 +86,6 @@ function AddWebsiteModule(name, url)
 end
 
 function Init()
-  AddWebsiteModule('HeavenManga', 'https://heavenmanga.org')
+  AddWebsiteModule('HeavenManga', 'http://ww2.heavenmanga.org')
   AddWebsiteModule('HolyManga', 'http://ww5.holymanga.net')
 end
