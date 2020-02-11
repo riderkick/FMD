98a9f9c57aef00fafab02926bf4b8f0be6b20211
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-02-11 13:44:12 +0800

    hitomila, fixed download (closed #599)

--- lua/modules/HitomiLa.lua
+++ lua/modules/HitomiLa.lua
@@ -25,45 +25,110 @@ function getinfo()
   end
 end
 
-function getpagenumber()
-  local subdomain, galleryid, prefix, s, x, v = nil
-  local isfixed, iswebp = false
-  
-  if http.get(MaybeFillHost(module.rooturl, url)) then
-	  galleryid = tonumber(ReplaceRegExpr('(?i)^.*reader/(\\d+).*$', url, '$1'))
-	  prefix = string.char(97 + (galleryid % 2)) .. 'a.'
-    if galleryid ~= nil then subdomain = prefix .. domain end
-    x = TXQuery.Create(http.Document)
-    v = x.xpath('//div[@class="img-url"]')
-    for i = 1, v.count do
-      s = v.get(i).tostring
-      if subdomain ~= nil then s = s:gsub('//[^/]+/', '//' .. subdomain .. '/') end
-      s = set_https(s)
-      
-	    if isfixed == false then
-        http.reset()
-        http.headers.values['Pragma'] = 'no-cache'
-        http.headers.values['Cache-Control'] = 'no-cache'
-        http.headers.values['Referer'] = MaybeFillHost(module.rooturl, url)
-	      if http.get(s) then
-	        err = TXQuery.Create(http.Document)
-	    	  e = err.xpathstring('//center/h1')
-	  	    if e == '403 Forbidden' then
-	  	      prefix = string.char(97 + (galleryid % 2) + 1) .. 'a.'
-	  	      subdomain = prefix .. domain
-	  	      s = v.get(i).tostring
-	  	      s = s:gsub('//[^/]+/', '//' .. subdomain .. '/')
-	  	      s = set_https(s)
-	  	    end
-	  	    isfixed = true
-	  	  end
-	    end
-      task.pagelinks.add(s)
-    end
-  else
-    return false
-  end
-  return true
+----------------------------------------------------------------------------------------------
+--  direct translaton of https://ltn.hitomi.la/common.js and https://ltn.hitomi.la/reader.js
+
+local adapose = false
+
+function subdomain_from_galleryid(g, number_of_frontends)
+        if (adapose) then
+                return '0'
+        end
+        local o = g % number_of_frontends
+
+		return string.char(97 + o)
+end
+
+function subdomain_from_url(url, base)
+        local retval = 'a'
+        if (base) then
+                retval = base
+        end
+        
+        local number_of_frontends = 3
+        local b = 16
+        
+        local r = '^.*/[0-9a-f]/([0-9a-f]{2})/.*$'
+        local m = re.replace(r,url,'$1')
+        if not(m) then
+                return retval
+        end
+        
+        local g = tonumber(m, b) or nil
+        if g then
+                if (g < 0x30) then
+                        number_of_frontends = 2
+                end
+                if (g < 0x09) then
+                        g = 1
+                end
+                retval = subdomain_from_galleryid(g, number_of_frontends) .. retval
+        end
+        
+        return retval
+end
+
+function url_from_url(url, base)
+		return re.replace('//..?\\.hitomi\\.la/', url, '//'..subdomain_from_url(url, base)..'.hitomi.la/')
+end
+
+
+function full_path_from_hash(hash)
+        if (hash:len() < 3) then
+                return hash
+        end
+        return re.replace('^.*(..)(.)$', hash, '$2/$1/'..hash)
+end
+
+
+function url_from_hash(galleryid, image, dir, ext)
+		ext = ext or dir or image.name:match('%.(.+)')
+        dir = dir or 'images'
+        
+        return 'https://a.hitomi.la/'..dir..'/'..full_path_from_hash(image.hash)..'.'..ext
+end
+
+function url_from_url_from_hash(galleryid, image, dir, ext, base) 
+        return url_from_url(url_from_hash(galleryid, image, dir, ext), base)
+end
+
+function image_url_from_image(galleryid, image, no_webp)
+        local webp
+        if (image['hash'] and image['haswebp'] and not(no_webp)) then
+                webp = 'webp'
+        end
+        
+        return url_from_url_from_hash(galleryid, image, webp)
+end
+
+-- end of https://ltn.hitomi.la/common.js
+----------------------------------------------------------------------------------------------
+
+function getpagenumber()  
+	if http.get(MaybeFillHost(module.rooturl, url)) then
+		x=TXQuery.Create(http.document)
+		local galleryid=x.xpathstring('//script[contains(.,"var galleryid")]'):match('=%s*(%d+)') or ''
+		local gallery_url=set_https(x.xpathstring('//script[contains(@src,"/galleries/")]/@src') or '')
+		if gallery_url and http.get(gallery_url) then
+			local no_webp=not module.GetOption('download_webp')
+			local s=StreamToString(http.document):match('(%[.+%])')
+			x.parsehtml(s)
+			local v=x.xpath('json(*)()')
+			local image={}
+			local vi
+			for i=1, v.count do
+				vi=v.get(i)
+				image.hash=x.xpathstring('./hash',vi)
+				image.haswebp=x.xpathstring('./haswebp',vi)=='1'
+				image.name=x.xpathstring('./name',vi)
+				image.hasavif=x.xpathstring('./hasavif',vi)=='1'
+				task.pagelinks.add(image_url_from_image(galleryid, image, no_webp))
+			end
+		end
+	else
+		return false
+	end
+	return true
 end
 
 function BeforeDownloadImage()
@@ -99,4 +164,5 @@ function Init()
   m.ongetpagenumber='getpagenumber'
   m.ongetnameandlink='getnameandlink'
   m.OnBeforeDownloadImage = 'BeforeDownloadImage'
+  m.AddOptionCheckBox('download_webp', 'Download WebP', true)
 end
