97893cf2dda44a652a62dbe24de641c46f9fe268
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-03-18 19:03:16 +0800

    mangadextoday, added mangalist

--- lua/modules/MangaDexToday.lua
+++ lua/modules/MangaDexToday.lua
@@ -1,3 +1,43 @@
+function Init()
+	m=NewModule()
+	m.Website                    = 'MangaDexToday'
+	m.RootURL                    = 'https://mangadex.today'
+	m.Category                   = 'English'
+	m.OnGetDirectoryPageNumber   = 'GetDirectoryPageNumber'
+	m.OnGetNameAndLink           = 'GetNameAndLink'
+	m.OnGetInfo                  = 'GetInfo'
+	m.OnGetPageNumber            = 'GetPageNumber'
+end
+
+local dirurl = '/popular-manga'
+
+function GetDirectoryPageNumber()
+	if http.get(module.rooturl .. dirurl) then
+		page = tonumber(TXQuery.create(http.document).x.xpathstring('//ul[@class="pagination"]/li[./a/@rel="next"]/preceding-sibling::li[1]/a')) or 1
+		return no_error
+	else
+		return net_problem
+	end
+end
+
+function GetNameAndLink()
+	local data = 'action=load_series_list_entries&parameter%5Bpage%5D=' .. IncStr(url) .. '&parameter%5Bletter%5D=&parameter%5Bsortby%5D=alphabetic&parameter%5Border%5D=asc'
+	local s = module.rooturl .. dirurl
+	if url ~= '0' then
+	  s = s .. '?page=' .. IncStr(url)
+	end
+	if http.get(s) then
+		local x = TXQuery.create(http.document)
+		local v for _,v in ipairs(x.xpathi('//li/div/div[@class="left"]/a')) do
+			links.add(v.getAttribute('href'))
+			names.add(x.xpathstring('./h2', v))
+		end
+		return no_error
+	else
+		return net_problem
+	end
+end
+
 function GetInfo()
 	mangainfo.url=MaybeFillHost(module.rooturl,url)
 	if http.get(mangainfo.url) then
@@ -27,13 +67,4 @@ function GetPageNumber()
 	else
 		return false
 	end
-end
-
-function Init()
-	m=NewModule()
-	-- m.category='English'
-	m.website='MangaDexToday'
-	m.rooturl='http://mangadex.today'
-	m.ongetinfo='GetInfo'
-	m.ongetpagenumber='GetPageNumber'
 end
