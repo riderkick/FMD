25fa6cc7ca57cbc641d9a434c957e9e6eeccc92e
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-04-10 07:33:37 +0800

    fixed copy command

--- make_release_win.bat
+++ make_release_win.bat
@@ -7,10 +7,10 @@ CD /D "%cdir%"
 SET repodl=https://github.com/fmd-project-team/FMD/releases/download/
 ECHO ; automatically build with make_release_win.bat>update
 
-CALL :makerelease i386-win32 Win32 --no-write-project -B
+CALL :makerelease i386-win32 Win32 --no-write-project
 ECHO WIN32=%repodl%%fverb%/%oname%>>update
 
-CALL :makerelease x86_64-win64 Win64 --no-write-project -B
+CALL :makerelease x86_64-win64 Win64 --no-write-project
 ECHO WIN64=%repodl%%fverb%/%oname%>>update
 
 ECHO VERSION=%fverb%>>update
@@ -20,7 +20,7 @@ GOTO :EOF
 
 :makerelease
 TITLE make %~1
-SET lbuild=%LAZ%\lazbuild --build-mode="%~2" %~3 %~4
+SET lbuild=%LAZ%\lazbuild --build-mode="%~2" %~3
 %lbuild% "%cdir%\updaterslim\updater.lpi"
 %lbuild% "%cdir%\mangadownloader\md.lpi"
 SET tdir=%cdir%\bin\%~1
@@ -35,11 +35,11 @@ call :copycdir extras
 call :copycdir images
 call :copycdir licenses
 XCOPY /C /F /Y "%cdir%\languages\*.po" "%odir%\languages\"
-XCOPY /E /C /F /S "%cdir%\dist\%~1" "%odir%\"
-COPY /Y "%tdir%\changelog.txt" "%odir%\"
-COPY /Y "%tdir%\readme.rtf" "%odir%\"
-COPY /Y "%tdir%\fmd.exe" "%odir%\"
-COPY /Y "%tdir%\updater.exe" "%odir%\"
+XCOPY /E /C /F /Y "%cdir%\dist\%~1" "%odir%\"
+XCOPY /F /Y "%cdir%\changelog.txt" "%odir%\"
+XCOPY /F /Y "%cdir%\readme.rtf" "%odir%\"
+XCOPY /F /Y "%tdir%\fmd.exe" "%odir%\"
+XCOPY /F /Y "%tdir%\updater.exe" "%odir%\"
 DEL /F "%rdir%\%oname%"
 "%cdir%\dist\%~1\7za" a "%rdir%\%oname%" "%odir%\*" -mx9 -ssw -stl -t7z -y
 RMDIR /S /Q "%odir%"
@@ -47,7 +47,7 @@ ECHO ----------------------------------------------------------
 GOTO :EOF
 
 :copycdir
-XCOPY /E /C /F /S  "%cdir%\%~1" "%odir%\%~1\"
+XCOPY /E /C /F "%cdir%\%~1" "%odir%\%~1\"
 GOTO :EOF
 
 :getfileversion
